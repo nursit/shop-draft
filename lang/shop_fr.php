8481eb56ebe629202967b9f15d10eef028d5decd
Cerdic <cedric@yterium.com>
2015-06-26 13:03:53 +0200

    panier et commande

--- lang/shop_fr.php
+++ lang/shop_fr.php
@@ -10,10 +10,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'prix' => 'Prix',
 	'reference' => 'Référence',
 	'produits_rubrique' => 'Les produits dans cette rubrique',
-	'votre_panier' => 'Mon panier',
 	'action_commander_panier' => 'Passer la commande',
-	'retour_achats' => 'Retourner à mes achats'
+	'retour_achats' => 'Retourner à mes achats',
+	'recapitulatif_commande' => 'Récapitulatif :',
+	'titre_votre_panier' => 'Mon panier',
+	'titre_votre_commande' => 'Ma commande #@id@',
+	'titre_aucune_commande' => 'Aucune commande',
+
+	'bouton_annuler_commande' => '<i class="icon-trash"></i> Annuler la commande',
+
+	'explication_aucune_commande_en_cours' => 'Vous n\'avez aucune commande en cours',
 
 );
-
-?>
