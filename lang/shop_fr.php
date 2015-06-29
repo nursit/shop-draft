5cab80dd67984c47ecc1b85307defab2181296fa
Cerdic <cedric@yterium.com>
2015-06-29 18:28:39 +0200

    ajout des pages livraison et paiement, fonctionnement de la page qui, breadcrumb sur le processus d'achat, chaines de langue...

--- lang/shop_fr.php
+++ lang/shop_fr.php
@@ -10,12 +10,30 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'prix' => 'Prix',
 	'reference' => 'Référence',
 	'produits_rubrique' => 'Les produits dans cette rubrique',
-	'action_commander_panier' => 'Passer la commande',
+	'action_commander_panier' => 'Commander',
+	'action_payer_commande' => 'Payer',
+	'action_choisir_livraison' => 'Choisir la livraison',
 	'retour_achats' => 'Retourner à mes achats',
 	'recapitulatif_commande' => 'Récapitulatif :',
+	'recapitulatif_commande_adresse_livraison' => 'Expédiée à&nbsp;:',
 	'titre_votre_panier' => 'Mon panier',
 	'titre_votre_commande' => 'Ma commande #@id@',
 	'titre_aucune_commande' => 'Aucune commande',
+	'titre_livraison' => 'Livraison',
+	'titre_paiement' => 'Paiement',
+
+	'titre_votre_adresse_livraison' => 'Votre adresse de livraison',
+	'bouton_modifier_adresse' => 'Modifier l\'adresse',
+
+	'titre_etape_panier' => 'Mon panier',
+	'titre_etape_qui' => 'Identification',
+	'titre_etape_commande' => 'Commande',
+	'titre_etape_livraison' => 'Livraison',
+	'titre_etape_paiement' => 'Paiement',
+
+	'explication_privacy_info_perso' => 'Ces informations sont uniquement utilisées pour gérer la livraison de votre commande,
+et ne seront jamais communiquées à des tiers. Nous ne pratiquons ni l\'achat, ni la vente, ni l\'échange de fichiers.',
+
 
 	'bouton_annuler_commande' => '<i class="icon-trash"></i> Annuler la commande',
 
--- mes_fonctions.php
+++ mes_fonctions.php
@@ -23,3 +23,37 @@ function affiche_prix($valeur,$decimales=2,$devise="EUR"){
 	}
 	return $price.$devise;
 }
+
+
+function shop_titre_etape($etape){
+	return _T('shop:titre_etape_'.$etape);
+}
+
+function shop_liste_etapes($id_panier){
+	static $lesetapes = array();
+	$etapes = array('panier','qui','commande','livraison','paiement');
+	if (!$id_panier) return $etapes;
+	if (isset($lesetapes["$id_panier"]))
+		return $lesetapes["$id_panier"];
+
+	if ($id_auteur = sql_fetsel('id_auteur','spip_paniers','id_panier='.intval($id_panier))){
+		$etapes = array_diff($etapes,array('qui'));
+	}
+
+	$items = sql_allfetsel("*","spip_paniers_liens","id_panier=".intval($id_panier));
+	$livrable = false;
+	foreach($items as $item){
+		$table = table_objet_sql($item['objet']);
+		$primary = id_table_objet($item['objet']);
+		$objet = sql_fetsel("*",$table,"$primary=".intval($item['id_objet']));
+		if (!isset($objet['immateriel']) OR !$objet['immateriel']){
+			$livrable = true;
+			break;
+		}
+	}
+	if (!$livrable){
+		$etapes = array_diff($etapes,array('livraison'));
+	}
+
+	return $lesetapes["$id_panier"] = $etapes;
+}
